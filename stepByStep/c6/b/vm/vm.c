acb557420772a77d33691f7813f92ed080c939da
meiken.gu <aluo_fei@163.com>
2023-10-30 15:21:37 +0800

    add: c6 - b

---
+++ stepByStep/c6/b/vm/vm.c
@@ -0,0 +1,24 @@
+#include "vm.h"
+#include "core.h"
+#include <stdlib.h>
+
+//初始化虚拟机
+void initVM(VM* vm) {
+    vm->allocatedBytes = 0;
+    vm->allObjects = NULL;
+    vm->curParser = NULL;
+    StringBufferInit(&vm->allMethodNames);
+    vm->allModules = newObjMap(vm);
+    vm->curParser = NULL;
+}
+
+VM* newVM() {
+    VM* vm = (VM*)malloc(sizeof(VM));
+    if (vm == NULL) {
+        MEM_ERROR("allocate VM failed!");
+    }
+    initVM(vm);
+    buildCore(vm);
+
+    return vm;
+}
